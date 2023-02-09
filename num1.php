eff43d5c3e469f9e755ffd4e86091b0c07a2a850
Frubbi <darinaign@mail.ru>
2023-02-09 10:02:26 +0300

    first programm

---
+++ num1.php
@@ -0,0 +1,17 @@
+<?php
+
+$product1 ='Стол';
+$product2 ='Стул';
+$product3 ='Столешница';
+
+$price1 ='2700';
+$price2 ='1500';
+$price3 ='2780';
+
+$awared =(int)(($price1+$price2+$price3)/3);
+
+print_r("\n{$product1}=> {$price1}руб");
+print_r("\n{$product2}=> {$price2}руб");
+print_r("\n{$product3}=> {$price3}руб");
+
+print_r("\n\nСредняя цена товаров={$awared}руб");
