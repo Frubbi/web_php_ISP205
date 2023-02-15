1ca9c72fa6f2d03246ed684cb9103c06d4f742a6
Frubbi <darinaign@mail.ru>
2023-02-15 20:22:58 +0300

    Оформила вывод данных о товаре в таблице

--- num1.php
+++ num1.php
@@ -9,6 +9,7 @@ $price2 ='1500';
 $price3 ='2780';
 
 $awared =(int)(($price1+$price2+$price3)/3);
+$average =round((($price1 + $price2 + $price3)/3), 2);
 
 print_r("\n{$product1}=> {$price1}руб");
 echo '<br>';
@@ -17,3 +18,8 @@ echo '<br>';
 print_r("\n{$product3}=> {$price3}руб");
 echo '<hr width="10%"align="left" >';
 print_r("\n\nСредняя цена товаров={$awared}руб");
+echo '<br>';
+echo '<br>';
+echo "<table border='1'><tr><td>Товар</td><td>Цена</td></tr><tr><td>$product1</td><td>$price1</td></tr><tr><td>$product2</td><td>$price2</td></tr><tr><td>$product3</td><td>$price3</td></tr></table>";
+echo '<br>';
+print_r("\n\nСредняя цена ={$average}руб.");
