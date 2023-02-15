3a22981fc5ebb2f2fbe07326f44ea6c6ef037a28
Frubbi <darinaign@mail.ru>
2023-02-15 19:46:16 +0300

    использовала элементы html

--- num1.php
+++ num1.php
@@ -11,7 +11,9 @@ $price3 ='2780';
 $awared =(int)(($price1+$price2+$price3)/3);
 
 print_r("\n{$product1}=> {$price1}руб");
+echo '<br>';
 print_r("\n{$product2}=> {$price2}руб");
+echo '<br>';
 print_r("\n{$product3}=> {$price3}руб");
-
+echo '<hr width="10%"align="left" >';
 print_r("\n\nСредняя цена товаров={$awared}руб");
